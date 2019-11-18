bef02dbe82f4661a83ff9947b1db46124c31db9f
Upson <UpsonP@dfo-mpo.ca>
2019-11-18 12:38:06 -0400

    Set url path to point to new PollList class

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.PollsList.as_view(), name='index'),
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
