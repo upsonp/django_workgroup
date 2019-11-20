bb8578f2125789cb74aeba0d6f40b3096b933124
Upson <UpsonP@dfo-mpo.ca>
2019-11-20 08:35:19 -0400

    end point for wg-2 session

--- polls/urls.py
+++ polls/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('list/question', views.QuestionsList.as_view(), name='question_list'),
+    path('new/question', views.QuestionCreate.as_view(), name='question_create'),
+    path('update/question/<int:pk>', views.QuestionUpdate.as_view(), name='question_update'),
+
+    path('new/choice', views.add_choice, name='add_choice'),
+    path('remove/choice', views.remove_choice, name='remove_choice'),
 ]
